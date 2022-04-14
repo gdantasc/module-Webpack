516656c5a033a7b955e05cfd2d59aa0a1cce99f6
gdantasc <gabrieldcavalcante00@gmail.com>
2022-04-14 13:52:19 -0300

    finish: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,51 @@
+const modoDev = process.env.NODE_ENV !== "production";
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: modoDev ? "development" : "production",
   entry: "./src/principal.js",
   output: {
     filename: "principal.js",
     path: __dirname + "/public",
   },
+  devServer: {
+    contentBase: "./public",
+    port: 9000,
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 6,
+        },
+      }),
+
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "estilo.css",
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.s?[ac]ss$/,
         use: [
-          "style-loader", //Adiciona dentro da dom a tag <style>
+          MiniCssExtractPlugin.loader,
+          // "style-loader", //Adiciona dentro da dom a tag <style>
           "css-loader", //interpreta @import, url()
+          "sass-loader",
         ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
       },
     ],
   },
